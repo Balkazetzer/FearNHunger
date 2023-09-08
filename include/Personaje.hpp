6eef898de6a58ada6b14bc26cd9000e139a30bca
Balkazetzer <143459548+Balkazetzer@users.noreply.github.com>
2023-09-08 23:30:09 +0000

    commit

--- include/Personaje.hpp
+++ include/Personaje.hpp
@@ -1,79 +1,16 @@
-#include <iostream>
-#include <cstdio>
-
 class Personaje
 {
+private:
+  /* data */
 public:
-  Personaje() {}
-  ~Personaje() {}
+  Personaje(/* args */);
+  ~Personaje();
 };
 
-class PersonajeJugable : public Personaje
+Personaje::Personaje(/* args */)
 {
-public:
-  PersonajeJugable() {}
-  ~PersonajeJugable() {}
-};
+}
 
-class PersonajeNoJugable : public Personaje
+Personaje::~Personaje()
 {
-public:
-  PersonajeNoJugable() {}
-  ~PersonajeNoJugable() {}
-};
-
-class Cahara : public PersonajeJugable
-{
-public:
-  Cahara() {}
-  ~Cahara() {}
-};
-
-class DArce : public PersonajeJugable
-{
-public:
-  DArce() {}
-  ~DArce() {}
-};
-
-class Ragnvaldr : public PersonajeJugable
-{
-public:
-  Ragnvaldr() {}
-  ~Ragnvaldr() {}
-};
-
-class Enki : public PersonajeJugable
-{
-public:
-  Enki() {}
-  ~Enki() {}
-};
-
-class Girl : public PersonajeNoJugable
-{
-public:
-  Girl() {}
-  ~Girl() {}
-};
-
-class LEgarde : public PersonajeNoJugable
-{
-public:
-  LEgarde() {}
-  ~LEgarde() {}
-};
-
-class Nashrah : public PersonajeNoJugable
-{
-public:
-  Nashrah() {}
-  ~Nashrah() {}
-};
-
-class Moonless : public PersonajeNoJugable
-{
-public:
-  Moonless() {}
-  ~Moonless() {}
-};
+}
